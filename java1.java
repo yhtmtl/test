a1c819b32e8a46ab1bc53fa35b1ce9153faea16a
yhtmtl <127288845+yhtmtl@users.noreply.github.com>
2023-03-09 19:30:08 +0900

    修改java1.java
    第二次提交

--- java1.java
+++ java1.java
@@ -1,6 +1,7 @@
 public static void main(String []args){
      String str = "\b\n\t";
         int sz;
+        int i =0;
         sz = str.length();
         for (int i = 0; i < sz; i++) {
             char ch = str.charAt(i);
